d64124c4518bc39de4cc7e07c09184bb9fa2db22
katy01101011 <katy.91560@gmail.com>
2022-04-24 14:42:08 +0200

    add: js: griglia "flessibile" che si apre al click

--- js/scritpt.js
+++ js/scritpt.js
@@ -1,11 +1,42 @@
+let difficult = 10; // Cambia il numero per cambiare la dimensione della griglia
+let limit = Math.pow(difficult, 2);
+    console.log(limit);
 let grid = document.querySelector('.grid');
-console.log(grid);
+    console.log(grid);
 
-let gridItem;
-for (i = 1; i <= 100; i++) {
-    gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    console.log(gridItem);
+document.getElementById('start').addEventListener('click', gridWithSquares);
 
-    grid.append(gridItem);
+// FUNCTIONS
+
+/**
+ * Description // ADD: n 'clickable' squares in a grid
+ * @returns {Object} // squares -> grid items
+ */
+function gridWithSquares() {
+    grid.innerHTML = ''; // Ripulisco griglia
+
+    for (let i = 1; i <= limit; i++) {
+        let gridItem = newGridSquare(i, difficult);
+        gridItem.addEventListener('click', function() {
+        this.classList.add('active');
+        })
+        grid.append(gridItem);
+    }
+
+    return grid;
+}
+
+/**
+ * Description // Generate n numer of squares
+ * @param {Number} spanText // Number to insert into the square
+ * @returns {Object} // Grid HTML item square with numer inside
+ */
+function newGridSquare(spanText, rowColQuantity) {
+    const newSquare = document.createElement("div");
+    newSquare.innerHTML = `<span>${spanText}</span>`;
+    newSquare.style.width = `calc(100% / ${rowColQuantity})`;
+    newSquare.style.height = `calc(100% / ${rowColQuantity})`;
+    newSquare.classList.add("grid-item");
+
+    return newSquare;
 }
